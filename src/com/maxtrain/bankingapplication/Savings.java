b9002e19862520063e864142b27f9855fca6ee45
Zach Tumbusch <zach.tumbusch@gmail.com>
2023-03-22 12:05:01 -0400

    addcalcint

--- src/com/maxtrain/bankingapplication/Savings.java
+++ src/com/maxtrain/bankingapplication/Savings.java
@@ -2,13 +2,22 @@ package com.maxtrain.bankingapplication;
 
 public class Savings extends Account{
 	public double interestRate = 0.12;
+	public double getInterestRate() {
+		return interestRate;
+	}
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
 	public double payInterest(int months)throws Exception {
-		var interest = getBalance() * (interestRate / 12) * months;
+		var interest = calculateInterest(months);
 		deposit(interest);
-		return interest;
-		
-		
-		
+		return interest;	
+	}
+	private double calculateInterest(int months) {
+		if(months < 1) {
+			return 0;
+		}
+		return getBalance() * (getInterestRate() /12) * months;
 	}
 	public Savings() {super();}
 	public Savings(String description) {super(description);}
